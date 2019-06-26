baaaa0b19698b43de4c266a3c2e480c909fcacad
Jochen <jochen.handels@web.de>
2019-06-26 16:27:33 +0200

    Added documentation and more feedback

--- backup-volume.sh
+++ backup-volume.sh
@@ -2,6 +2,16 @@
 
 set -eu
 
+function usage {
+  cat <<EOM
+usage: $(basename $0) [--container=<container_name>] <image_name>*
+  --container   specify container to stop during backup
+EOM
+  exit 1
+}
+
+[ -z $1 ] && { usage; }
+
 container_name=
 
 for arg do
@@ -18,18 +28,18 @@ done
 if [ ! -z "$container_name" ]
 then
   echo "Stopping container '$container_name'..."
-  docker stop "$container_name" && echo "Stopped"
+  docker stop "$container_name" && echo "Stopped" || >&2 echo "Failed"
 fi
 
-cd /source &&
+cd /source
 
 for volume_name do
   echo "Backing up volume '$volume_name'..."
-  tar cfz "/target/$volume_name.tar.gz" "$volume_name" && echo "Done"
+  tar cfz "/target/$volume_name.tar.gz" "$volume_name" && echo "Done" || >&2 echo "Failed"
 done
 
 if [ ! -z $container_name ]
 then
   echo "Starting container '$container_name'..."
-  docker start "$container_name" && echo "Started"
+  docker start "$container_name" && echo "Started" || >&2 echo "Failed"
 fi
--- restore-volume.sh
+++ restore-volume.sh
@@ -2,6 +2,16 @@
 
 set -eu
 
+function usage {
+  cat <<EOM
+usage: $(basename $0) [--container=<container_name>] <image_name>*
+  --container   specify container to stop during restore
+EOM
+  exit 1
+}
+
+[ -z $1 ] && { usage; }
+
 container_name=
 
 for arg do
@@ -18,18 +28,20 @@ done
 if [ ! -z "$container_name" ]
 then
   echo "Stopping container '$container_name'..."
-  docker stop "$container_name" && echo "Stopped"
+  docker stop "$container_name" && echo "Stopped" || >&2 echo "Failed"
 fi
 
-cd /source &&
+cd /target
 
 for volume_name do
+  echo "Clearing volume '$volume_name'..."
+  rm -rf "$volume_name/*" "$volume_name/..?*" "$volume_name/.[!.]*" && echo "Done" || >&2 echo "Failed" 
   echo "Restoring volume '$volume_name'..."
-  tar xfz "/target/$volume_name.tar.gz" "$volume_name" && echo "Done"
+  tar xfz "/source/$volume_name.tar.gz" "$volume_name" && echo "Done" || >&2 echo "Failed"
 done
 
 if [ ! -z $container_name ]
 then
   echo "Starting container '$container_name'..."
-  docker start "$container_name" && echo "Started"
+  docker start "$container_name" && echo "Started" || >&2 echo "Failed"
 fi
